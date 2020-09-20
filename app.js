09baca97f2a7f3cc8e469f61f91762ffd0831a76
Bhattarai <narayanbhattarai@live.com>
2020-09-20 18:41:37 -0400

    My first node.js/javascrip and mongodb project using express

---
+++ app.js
@@ -0,0 +1,11 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const router = require("./router");
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use("/", router);
+app.listen("3000");
